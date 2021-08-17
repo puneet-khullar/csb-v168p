e9d954c3b07fedf804a2e2c08add84947741c7ae
Code Sandbox <hello@codesandbox.io>
2021-08-17 10:14:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e21d1458bc16c4bf98e6f3b2440bd7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e61c041c8607e88454d5328530f11c",
+    "url": "https://puneet-khullar.github.io/csb-v168p/index.html"
+  },
+  {
+    "revision": "6779aef8db4999e40f48",
+    "url": "https://puneet-khullar.github.io/csb-v168p/static/css/main.d888f8bf.chunk.css"
+  },
+  {
+    "revision": "05208fbf04931bc303b7",
+    "url": "https://puneet-khullar.github.io/csb-v168p/static/js/2.0c7ea6fb.chunk.js"
+  },
+  {
+    "revision": "2c13bc363f4a9e16bf86dc4b31ae6f68",
+    "url": "https://puneet-khullar.github.io/csb-v168p/static/js/2.0c7ea6fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6779aef8db4999e40f48",
+    "url": "https://puneet-khullar.github.io/csb-v168p/static/js/main.aee1f833.chunk.js"
+  },
+  {
+    "revision": "0c7a959eeb29c5f6e824",
+    "url": "https://puneet-khullar.github.io/csb-v168p/static/js/runtime-main.f588f40e.js"
+  }
+]);
